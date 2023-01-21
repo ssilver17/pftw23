302a56bc1f5afef64a1d568e64a84a3873d8b769
ssilver17 <kstowesilver@gmail.com>
2023-01-21 11:26:48 -0600

    add hat

--- week1/assign/snowman.js
+++ week1/assign/snowman.js
@@ -10,11 +10,21 @@ function draw() {
     //snowman bottom circle
     fill("#caf9fa");
     stroke("#1e2928");
+    strokeWeight(5);
     ellipse(600, 700, 300, 250);
     //middle circle
     ellipse(600, 500, 225, 200);
     //head
     ellipse(600, 325, 175);
+    //hat brim
+    stroke("#455e5c");
+    strokeWeight(20);
+    line(500, 250, 690, 225);
+    //hat shape
+    strokeWeight(8);
+    fill("#455e5c")
+    quad(550, 250, 525, 180, 675, 200, 700, 225)
+
 
 
 }
