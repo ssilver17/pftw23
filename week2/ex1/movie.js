ddd284ad002be6b12958de88503cb66fc1be980a
ssilver17 <kstowesilver@gmail.com>
2023-01-27 18:45:01 -0600

    add prompt and toString

--- week2/ex1/movie.js
+++ week2/ex1/movie.js
@@ -1,2 +1,7 @@
 const myMovies = ["Moonstruck", "But I'm a Cheerleader", "Call Me by Your Name", "The Matrix", "Say Anything"];
 console.log(myMovies);
+let yourMovie = prompt("What is the name of your favorite movie?");
+myMovies.push(yourMovie);
+console.log("My movies list updated", myMovies);
+console.log(myMovies.toString());
+alert("Our movies are" + myMovies.toString());
