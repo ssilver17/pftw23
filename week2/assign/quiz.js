28dc1a57c329bafd883c87fd6fd31134b99f7a09
ssilver17 <kstowesilver@gmail.com>
2023-01-29 15:56:14 -0600

    update questions

--- week2/assign/quiz.js
+++ week2/assign/quiz.js
@@ -1,6 +1,10 @@
 const questions = [
+    {question: "What are the two major Native American tribes that live in the area that is now Minnesota",
+    answer: "Dakota and Anishinabe"},
+    {question: "How many miles of rivers and streams are there is Minnesota?",
+    answer: "69,200 miles"},
     {question: "Minnesota shares geographic boundaries with how many sovereign Native Nations?",
     answer: "11"},
-    {question: "Minnesota shares geographic boundaries with how many sovereign Native Nations?",
-    answer: "11"},
+    {question: "In what state does the Mississippi River begin?",
+    answer: "Minnesota"},
 ]
