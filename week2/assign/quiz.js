f8a4f91edbc0cab04f0277948acd951aa06c5f83
ssilver17 <kstowesilver@gmail.com>
2023-01-29 15:19:45 -0600

    adding questions

---
+++ week2/assign/quiz.js
@@ -0,0 +1,6 @@
+const questions = [
+    {question: "Minnesota shares geographic boundaries with how many sovereign Native Nations?",
+    answer: "11"},
+    {question: "Minnesota shares geographic boundaries with how many sovereign Native Nations?",
+    answer: "11"},
+]
