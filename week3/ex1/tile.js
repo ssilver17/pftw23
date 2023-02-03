2f31bd6bd5d2a9099e2c933d27544b1590044ddc
ssilver17 <kstowesilver@gmail.com>
2023-02-02 20:14:19 -0600

    start tile

---
+++ week3/ex1/tile.js
@@ -0,0 +1,12 @@
+//let rotateBy = 5;
+function setup() {
+    createCanvas(200, 200);
+}
+function createTile() {
+    translate(0,0);
+    fill("#3798b9");
+    rect(0, 0, 200, 200);
+}
+function draw() {
+    createTile();
+}
