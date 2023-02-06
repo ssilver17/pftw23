36cfd7003bf354fe25b84e9c23d53d6d70c8499d
ssilver17 <kstowesilver@gmail.com>
2023-02-05 23:42:26 -0600

    finish

--- week3/assign/radial.js
+++ week3/assign/radial.js
@@ -1,27 +1,36 @@
 let rotateBy = 5
-//let fillColor = (25, 215, 232)
 function setup() {
     createCanvas(600, 600);
     background(0);
     angleMode(DEGREES);
 }
 
-function makeArm(rotateBy) {
-    let alt = Math.round(rotateBy / 180);
-    fill("orange");
+function makeArm(rotateBy, fillColor, strokeSize) {
+    let alt = Math.round(rotateBy / 45);
+    fill(fillColor);
     stroke(255);
-    strokeWeight(2);
-    square(50, 50 + alt, 100);
-    square(0, 0 - alt, 100);
+    strokeWeight(strokeSize);
+    square(50, 0 - alt, 100);
+    
 }
 
 function draw() {
-    translate(360, 360);  //resets the center
+    translate(400, 400);  //resets the center
     rotate(rotateBy);  //rotates canvas
-    makeArm(rotateBy);
+    makeArm(rotateBy, "orange", 2);
     rotateBy += 200;
     rotateBy += 50;
-
+    scale(0.5)
+    makeArm(rotateBy, "lightblue", 10);
+    scale(0.25)
+    makeArm(rotateBy, "hotpink", 20);
+    rotateBy += 200;
+    rotateBy += 50;
+    scale(1)
+    makeArm(rotateBy, "hotpink", 50);
+    scale(0.5)
+    makeArm(rotateBy, "black");
+    
 }
 function mousePressed() {
     noLoop();
