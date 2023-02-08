a20dd4163fc04a85f26a37b909673abd9b9119e7
ssilver17 <kstowesilver@gmail.com>
2023-02-08 14:56:04 -0600

    added a second empty div

--- week4/ex1/dom.js
+++ week4/ex1/dom.js
@@ -8,3 +8,12 @@ myEmptyDiv.appendChild(heading);
 function handleHeadingClick() {
     document.body.style.backgroundColor = "orange";
 }
+
+const emptyDivTwo = document.querySelector("#emptyDivTwo");
+const subhead = document.createElement("h2");
+subhead.innerHTML = "Ancestors of the llama lived in the Great Plains of North America";
+subhead.addEventListener("mouseover", handleSubheadClick);
+emptyDivTwo.appendChild(subhead);
+function handleSubheadClick() {
+    document.body.style.color = "blue"; 
+}
