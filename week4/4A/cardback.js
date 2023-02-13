a9e117216dd882e445106984a456688ae897b28b
ssilver17 <kstowesilver@gmail.com>
2023-02-12 21:14:32 -0600

    card back design

--- week4/4A/cardback.js
+++ week4/4A/cardback.js
@@ -5,5 +5,18 @@ background("#042152");
 
 function draw() {
     fill("#bef2f9");
+    noStroke();
     ellipse(200, 200, 150);
+    fill("#ffba10");
+    noStroke();
+    triangle(200, 140, 240, 200, 160, 200);     //top triangle
+    triangle(200, 260, 160, 200, 240, 200);     //bottom triangle
+    noFill();
+    stroke("#ffba10");
+    strokeWeight(2)
+    ellipse(200, 200, 120);
+    ellipse(200, 200, 80, 120);
+    ellipse(200, 200, 60, 120);
+    ellipse(200, 200, 40, 100);
+    ellipse(200, 200, 120, 60);
 }
--- week4/4A/memory.js
+++ week4/4A/memory.js
@@ -8,24 +8,25 @@ function setup() {
     createCanvas(900, 1000);
     background("#042152");
     fill("#bef2f9");
+    noStroke();
     //ellipse(circleX, circleY, circleDiameter);
-    for(let j = 0; j < 4; j++) {
-        for(let i = 0; i < 4; i++) {
+    for(let j = 0; j < 4; j++) {            //array for rows 2-4
+        for(let i = 0; i < 4; i++) {        //array for fisrt row
             ellipse(startX, startY, circleDiameter);
-            startX += 200;
+            startX += 200;                  //horiz gap between circles
     }    
-    startY += 200;
-    startX = 150;
+    startY += 200;                          //vertical gap between circles
+    startX = 150;                           //x position for row starts 2-4
     }   
 }
 
 function draw() {
     fill("#ffba10");
     noStroke();
-    quad(0, 0, 600, 0, 675, 125, 0, 125);
+    quad(0, 0, 600, 0, 675, 125, 0, 125);   //header
     fill("#9a700a");
     noStroke();
-    quad(604, 0, 1000, 0, 1000, 125, 679, 125);
+    quad(604, 0, 1000, 0, 1000, 125, 679, 125);     //tally
 
     
     
