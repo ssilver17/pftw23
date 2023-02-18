7ddfa16ab87624bb2bd4e7971b5680350fde678f
ssilver17 <kstowesilver@gmail.com>
2023-02-18 16:08:44 -0600

    Update memory.js

--- week5/assign/memory.js
+++ week5/assign/memory.js
@@ -1,3 +1,7 @@
+let myCard;
+const DOWN = "down";
+const UP = "up";
+
 function setup() {
     createCanvas(900, 1000);
     background("#042152");
@@ -5,11 +9,16 @@ function setup() {
     let myCard = new Card(); 
 }
 
+function mousePressed() {
+    console.log(myCard.didHit(mouseX, mouseY));
+}
+
 class Card {
     constructor() {
         this.x = 150;
         this.y = 250;
         this.diameter = 150;
+        this.face = "down";
         this.show();
     }
 
@@ -17,5 +26,14 @@ class Card {
         fill("#bef2f9");
         ellipse(this.x, this.y, this.diameter);
     }
-}
 
+    didHit (mouseX, mouseY) {
+        if (mouseX >= this.x && mouseX <= this.x + this.diameter && 
+            mouseY >= this.y && mouseY <= this.y + this.diameter) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
