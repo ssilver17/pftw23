d59761c3e726426ebde0bf4d944d0076da75af40
ssilver17 <kstowesilver@gmail.com>
2023-02-28 21:50:38 -0600

    add v-bind href

--- week7/ex1/compare.js
+++ week7/ex1/compare.js
@@ -3,6 +3,8 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
+            llamaUrl: "https://en.wikipedia.org/wiki/Llama",
+            alpacaUrl: "https://en.wikipedia.org/wiki/Alpaca",
             linkStyle: {
                 background: "orange",  
                 padding: "10px" 
