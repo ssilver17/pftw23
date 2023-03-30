8c55743edf3c67d68bcb7cdc75a1cbd1038dc3d6
ssilver17 <kstowesilver@gmail.com>
2023-03-30 14:54:52 -0500

    add nav

--- week10/ex1/src/App.vue
+++ week10/ex1/src/App.vue
@@ -13,6 +13,59 @@
   </div>
 </template>
 
-<style scoped>
+<style>
+  html {
+    font-size: 16px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    margin: 50px 400px;
+  }
+
+  #app {
+    margin: 0;
+    padding: 0;
+  }
+
+  nav {
+    text-align: center;
+    width: 100%;
+    height: 40px;
+    background: rgb(190, 151, 11);
+  }
+
+  nav a, nav a:hover, nav a:link, nav a:active, nav a:visited {
+    color: aliceblue;
+    font-weight: 900;
+    font-size: 20px;
+    line-height: 2;
+    text-decoration: none;
+  }
+
+  nav a.router-link-active {
+    text-decoration: underline;
+    color: rgb(2, 8, 29);
+  }
+
+  .container h1 {
+    text-align: center;
+    font-size: 48px;
+    font-weight: bold;
+  }
+
+  .container p {
+    font-size: 16px;
+  }
+  .container {
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 10px;
+  }
+
+  .container img {
+    display: block;
+    max-width: 400px;
+    width: 100%;
+    margin: 20px auto 20px;
+  }
 
 </style>
