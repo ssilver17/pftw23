9e262f9a3bcdff85057bf43ced71f83a0506a4eb
ssilver17 <kstowesilver@gmail.com>
2023-03-30 21:18:02 -0500

    adding style

---
+++ week10/ex2/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import MusicList from "../data/music-list.json";
+console.log("Music list" , MusicList);
+</script>
+
+<template>
+<div class="song-container">
+    <div class="song-list-item" v-for="(song, index) in MusicList" :key="index">
+        <RouterLink :to="'/song/' + song.slug">{{ song.title }}</RouterLink>
+    </div>  
+</div>
+  
+</template>
---
+++ week10/ex2/src/views/Song.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import MusicList from "../data/music-list.json"
+const route = useRoute();
+console.log(route.params.slug);
+const song = MusicList.find(song => {
+    return song.slug === route.params.slug
+})
+</script>
+
+<template>
+    <div class="song-detail" :class="{
+        rising: song.rank > song.position.positionLastWeek, falling: song.rank < song.positionLastWeek, holding: song.rank === song.position.positionLastWeek }">
+        <h1>{{ song.position }}</h1>
+        <h2>{{ song.title }}</h2>
+        <img :src="song.cover" :alt="song.title" />
+        <p>Artist: {{ song.artist }}</p>
+        <p></p>
+    </div>
+</template>
+
+<style scoped>
+.song-detail .rising {
+    border: 1px solid rgb(242, 15, 185);
+}
+
+.song-detail .falling {
+    border: 1px solid rgb(5, 36, 90);
+}
+
+.song-detail .holding {
+    border: 1px dashed orange;
+}
+</style>
