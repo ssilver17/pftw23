cfc55d6ca6db3152641b379b0846e843c222fb96
ssilver17 <kstowesilver@gmail.com>
2023-04-09 20:02:49 -0500

    update angle

---
+++ week11/assign/angle.js
@@ -0,0 +1,37 @@
+
+
+function setup() {
+    createCanvas(600, 600);
+    background(18, 68, 159);
+    frameRate(5)
+}
+
+function redAngle() {
+    strokeWeight(3);
+    stroke(212, 29, 82);
+    for (var x = 1; x < 600; x +=5) {
+    line(x, 0, x, 235);
+    line(x, 0, x, 235);
+    }
+}
+
+
+function blueAngle() {
+    strokeWeight(1);
+    stroke(18, 68, 159);
+    for (var x = 10; x < 600; x +=5) {
+    line(0, 225, x, 0);
+    line(x, 550, x, 200);
+    }
+}
+
+function draw() {
+    translate(random(50), random(600));
+    redAngle();
+    blueAngle();
+}
+
+
+function mousePressed() {
+    noLoop();
+ }
