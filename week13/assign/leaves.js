afb65a1b9ddb6ff371b6f9fc597175d54c2d248a
ssilver17 <kstowesilver@gmail.com>
2023-04-23 21:25:39 -0500

    Update leaves.js

--- week13/assign/leaves.js
+++ week13/assign/leaves.js
@@ -5,66 +5,61 @@ var angle = 0.0;
 function setup() {
     createCanvas(1200, 800);
     angleMode(DEGREES);
-    frameRate(1);
+    frameRate(3);
 
     pos = 0.0
     inc = 2.0
 }
 
 function draw() {
-    background(99, 231, 295);
+    background(12, 31, 60);
 
     tree();
-    randomSeed(0);
-    for (var i = 100; i < width + 100; i += 10) {
-        var scalar = random(0.25, 1.0);
-        leafA(i, 110, scalar);
-    }
-    for (var j = 100; j < width + 100; j += 20) {
-        var scalar = random(0.25, 1.0);
-        leafB(j, 100, scalar);
-    }
-
-    
+    randomSeed(99);
+    for (var i = 0; i < width; i += 5) {
+        var scalar = random(0.05, .75);
+        dotA(i, 0, scalar);
+    }    
 }
 
 function tree() {
     fill(78, 55, 18);
     noStroke();
     rect(0, 0, 225, 800);
-    fill(99, 231, 295);
-    ellipse(225, 300, 200, 1000);
+    fill(12, 31, 60);
+    ellipse(225, 300, 200, 1000);   //tree trunk
+
+    fill(64, 123, 45);              //leaves
+    ellipse(100, 100, 400, 500);
+
+    fill(61, 112, 47, 200);
+    ellipse(300, 50, 400, 300);
+
+    fill(83, 210, 26, 150);
+    ellipse(300, 200, 300, 300);
+
+    fill(73, 139, 15, 200);
+    ellipse(475, 100, 200, 300);
+
+    fill(61, 112, 47, 150);
+    ellipse(600, 50, 200, 200);
 }
 
-function leafA(x, y, s) {
+function dotA(x, y, s) {
     push();
     translate(x, y);
     scale(s);
     var mySinVal = sin(pos);
     amplified = mySinVal * 10;
     
-    fill(77, 171, 72);
-    noStroke();
-    rotate(15); 
-    ellipse(300, 100, amplified, 50);
-    pos = pos + inc;
-    pop();
-}
-
-function leafB(x, y, s) {
-    push();
-    translate(x, y);
-    scale(s);
-    var mySinVal = sin(pos);
-    amplified = mySinVal * 10;
-    
-    fill(49, 94, 41);
+    fill(255, 242, 99);
     noStroke();
     rotate(30); 
-    ellipse(100, 100, amplified, 75);
+    ellipse(1000, 800, amplified, 10);
     pos = pos + inc;
     pop();
 }
+
 
 
 
